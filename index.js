2239c5642f84b3ac1f18caecd4a76c8e3c397540
mmccarthy64 <mmccarthy64@hotmail.com>
2021-05-18 14:17:33 -0500

    commit tree 8ad015087d331896624fc3dbbb12972c2eb58e26

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(drivers, sought) {
+    return drivers.filter(name => 
+        name.toLowerCase() === sought.toLowerCase()
+    )
+}
+
+function fuzzyMatch(drivers, search) {
+    return drivers.filter(match=>
+        match.toLowerCase().indexOf(search.toLowerCase()) === 0
+        )
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(match => match.name === name);
+}
